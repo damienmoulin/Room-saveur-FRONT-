bfb19a69e89049a622eb13cedfd0bf0383c6cf26
Jacques tatiana <tatout08090@gmail.com>
2016-10-13 16:14:16 +0200

    add page contenu

--- eCommerce/app/assets/scripts/app.js
+++ eCommerce/app/assets/scripts/app.js
@@ -1,7 +1,41 @@
 //module menu
 
-$(document).ready(function(){
+var initMore = function () {
+    $('.btn-act').on('click', function () {
+        var isMore = $(this).hasClass('more');
+        var parent = $(this).parent();
+        var number = $(parent).find('.number').html();
+        var $number = $(parent).find('.number');
+        number = parseInt(number);
+
+
+        if (isMore) {
+            var verif = number + 1;
+
+            if(verif > 20){
+                $number.html(20);
+            }else{
+                $number.html(verif);
+            }
+        } else {
+
+            var verif = number - 1;
+            if(verif < 0){
+                $number.html(0);
+            }else{
+                $number.html(verif);
+            }
+        }
+    })
+}
+
+
+$(document).ready(function () {
 
     console.log('ok');
-    
+
+    if ($('.btn-act').length > 0) {
+        initMore();
+    }
+
 });
